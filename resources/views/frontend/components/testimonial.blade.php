fb40becb6743dff8f6835c824cd819ef55dd9198
Usama Hassan <ahmarkhan1233456@gmail.com>
2024-01-20 15:39:12 +0500

    index, images and styling

---
+++ resources/views/frontend/components/testimonial.blade.php
@@ -0,0 +1,104 @@
+<!-- START:: Testimonials -->
+<section class="pc-testimonial">
+      <div class="testimonial-slider">
+        <div id="carouselExampleControls" class="carousel carousel-dark">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-4">
+                <div class="testimonial-title">
+                  <h2 class="fw-bold display-6">What our customers say?</h2>
+                </div>
+                <div>
+                  <p class="text-white">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, eaque? Doloremque provident labore accusantium nostrum architecto? Dolore, sed. Explicabo ipsum et accusantium odio quis hic vero ea obcaecati ullam laboriosam?</p>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+              <div class="col-md-8">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <div class="card w-75 mx-auto ">
+                      <div class="img-wrapper"><img src="{{asset('frontend/assets/images/person/jasmine.jpg')}}" class="d-block w-100" alt="..."> </div>
+                      <div class="card-body">
+                        <h5 class="card-title">Mubeen Akbar</h5>
+                        <i class="fa fa-star text-warning pe-1"></i>
+                        <i class="fa fa-star text-warning pe-1"></i>
+                        <i class="fa fa-star text-warning pe-1"></i>
+                        <i class="fa fa-star text-warning pe-1"></i>
+                        <i class="fa fa-star text-warning pe-1"></i>
+                        <p class="card-text">Some quick example text to build on the card title and make up
+                          the bulk of the
+                          card's content.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <div class="card w-75 mx-auto">
+                      <div class="img-wrapper"><img src="{{asset('frontend/assets/images/person/momina.jpg')}}" class="d-block w-100" alt="..."> </div>
+                      <div class="card-body">
+                        <h5 class="card-title">Mehwish Mushtaq</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up
+                          the bulk of the
+                          card's content.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <div class="card w-75 mx-auto">
+                      <div class="img-wrapper"><img src="{{asset('frontend/assets/images/person/nisma.jpg')}}" class="d-block w-100" alt="..."> </div>
+                      <div class="card-body">
+                        <h5 class="card-title">Saima Asad</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up
+                          the bulk of the
+                          card's content.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <div class="card w-75 mx-auto">
+                      <div class="img-wrapper"><img src="{{asset('frontend/assets/images/person/mewhish.jpg')}}" class="d-block w-100" alt="..."> </div>
+                      <div class="card-body">
+                        <h5 class="card-title">Momina Hassan</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up
+                          the bulk of the
+                          card's content.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <div class="card w-75 mx-auto">
+                      <div class="img-wrapper"><img src="{{asset('frontend/assets/images/person/tahir.jpg')}}" class="d-block w-100" alt="..."> </div>
+                      <div class="card-body">
+                        <h5 class="card-title">Tahir Khadim</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up
+                          the bulk of the
+                          card's content.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <div class="card w-75 mx-auto">
+                      <div class="img-wrapper"><img src="{{asset('frontend/assets/images/person/afnan.jpg')}}" class="d-block w-100" alt="..."> </div>
+                      <div class="card-body">
+                        <h5 class="card-title">Afnan Safder</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up
+                          the bulk of the
+                          card's content.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </section>
+    <!-- END:: Testimonials -->
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -282,7 +282,7 @@
         <div class="item">
           <!-- card -->
           <div class="card" data-aos="fade-up" data-aos-delay="1000">
-            <img onclick="window.location.href='airport.html'" class="card-img-top img-fluid" src="assets/images/900x600/1.jpg" alt="Card image" style="width:100%;">
+            <img class="card-img-top img-fluid" src="{{asset('frontend/assets/images/900x600/1.jpg')}}" alt="Card image" style="width:100%;">
             <div class="card-body">
               <h5 class="card-title">Airport Transfer</h5>
               <p class="card-text">
@@ -296,7 +296,7 @@
         <div class="item">
           <!-- card -->
           <div class="card" data-aos="fade-down" data-aos-delay="1000">
-            <img onclick="window.location.href='wedding.html'" class="card-img-top img-fluid" src="assets/images/900x600/2.jpg" alt="Card image" style="width:100%;">
+            <img class="card-img-top img-fluid" src="{{asset('frontend/assets/images/900x600/2.jpg')}}" alt="Card image" style="width:100%;">
             <div class="card-body">
               <h5 class="card-title">Wedding</h5>
               <p class="card-text">
@@ -310,7 +310,7 @@
         <div class="item">
           <!-- card -->
           <div class="card" data-aos="fade-up" data-aos-delay="1000">
-            <img onclick="window.location.href='events.html'" class="card-img-top img-fluid" src="assets/images/900x600/3.jpg" alt="Card image" style="width:100%;">
+            <img class="card-img-top img-fluid" src="{{asset('frontend/assets/images/900x600/3.jpg')}}" alt="Card image" style="width:100%;">
             <div class="card-body">
               <h5 class="card-title">Events</h5>
               <p class="card-text">
@@ -324,7 +324,7 @@
         <div class="item">
           <!-- card -->
           <div class="card" data-aos="fade-down" data-aos-delay="1000">
-            <img onclick="window.location.href='business.html'" class="card-img-top img-fluid" src="assets/images/900x600/4.jpg" alt="Card image" style="width:100%;">
+            <img onclick="window.location.href='business.html'" class="card-img-top img-fluid" src="{{asset('frontend/assets/images/900x600/4.jpg')}}" alt="Card image" style="width:100%;">
             <div class="card-body">
               <h5 class="card-title">Business</h5>
               <p class="card-text">
@@ -337,9 +337,8 @@
         <!-- 5 -->
         <div class="item">
           <!-- card -->
-          <div class="card" data-aos="fade-up" data-aos-delay="1000"
-          >
-            <img onclick="window.location.href='tours.html'" class="card-img-top img-fluid" src="assets/images/900x600/5.jpg" alt="Card image" style="width:100%;">
+          <div class="card" data-aos="fade-up" data-aos-delay="1000">
+            <img class="card-img-top img-fluid" src="{{asset('frontend/assets/images/900x600/5.jpg')}}" alt="Card image" style="width:100%;">
             <div class="card-body">
               <h5 class="card-title">Tours</h5>
               <p class="card-text">
@@ -404,111 +403,7 @@
       </div>
     </section>
     <!-- END:: Benam Section -->
-    
-    <!-- START:: Testimonials -->
-    <section class="pc-testimonial">
-      <div class="testimonial-slider">
-        <div id="carouselExampleControls" class="carousel carousel-dark">
-          <div class="container-fluid">
-            <div class="row">
-              <div class="col-md-4">
-                <div class="testimonial-title">
-                  <h2 class="fw-bold display-6">What our customers say?</h2>
-                </div>
-                <div>
-                  <p class="text-white">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, eaque? Doloremque provident labore accusantium nostrum architecto? Dolore, sed. Explicabo ipsum et accusantium odio quis hic vero ea obcaecati ullam laboriosam?</p>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Next</span>
-                </button>
-              </div>
-              <div class="col-md-8">
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <div class="card w-75 mx-auto ">
-                      <div class="img-wrapper"><img src="assets/images/person/jasmine.jpg" class="d-block w-100" alt="..."> </div>
-                      <div class="card-body">
-                        <h5 class="card-title">Mubeen Akbar</h5>
-                        <i class="fa fa-star text-warning pe-1"></i>
-                        <i class="fa fa-star text-warning pe-1"></i>
-                        <i class="fa fa-star text-warning pe-1"></i>
-                        <i class="fa fa-star text-warning pe-1"></i>
-                        <i class="fa fa-star text-warning pe-1"></i>
-                        <p class="card-text">Some quick example text to build on the card title and make up
-                          the bulk of the
-                          card's content.</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="carousel-item">
-                    <div class="card w-75 mx-auto">
-                      <div class="img-wrapper"><img src="assets/images/person/momina.jpg" class="d-block w-100" alt="..."> </div>
-                      <div class="card-body">
-                        <h5 class="card-title">Mehwish Mushtaq</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up
-                          the bulk of the
-                          card's content.</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="carousel-item">
-                    <div class="card w-75 mx-auto">
-                      <div class="img-wrapper"><img src="assets/images/person/nisma.jpg" class="d-block w-100" alt="..."> </div>
-                      <div class="card-body">
-                        <h5 class="card-title">Saima Asad</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up
-                          the bulk of the
-                          card's content.</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="carousel-item">
-                    <div class="card w-75 mx-auto">
-                      <div class="img-wrapper"><img src="assets/images/person/mewhish.jpg" class="d-block w-100" alt="..."> </div>
-                      <div class="card-body">
-                        <h5 class="card-title">Momina Hassan</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up
-                          the bulk of the
-                          card's content.</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="carousel-item">
-                    <div class="card w-75 mx-auto">
-                      <div class="img-wrapper"><img src="assets/images/person/tahir.jpg" class="d-block w-100" alt="..."> </div>
-                      <div class="card-body">
-                        <h5 class="card-title">Tahir Khadim</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up
-                          the bulk of the
-                          card's content.</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="carousel-item">
-                    <div class="card w-75 mx-auto">
-                      <div class="img-wrapper"><img src="assets/images/person/afnan.jpg" class="d-block w-100" alt="..."> </div>
-                      <div class="card-body">
-                        <h5 class="card-title">Afnan Safder</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up
-                          the bulk of the
-                          card's content.</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      
-    </section>
-    <!-- END:: Testimonials -->
+    @include('frontend.components.testimonial')
     @include('frontend.components.newsletter')
 @endsection
 @section('custom-js')
