ae7642c732638d5adbb4f3046500994aca2e8686
Usama Hassan <ahmarkhan1233456@gmail.com>
2024-01-23 17:55:44 +0500

    Service pages top component

--- resources/views/frontend/airport.blade.php
+++ resources/views/frontend/airport.blade.php
@@ -18,21 +18,7 @@
 
   <!-- End Hero Section -->
 <!-- ========== Start Section first  ========== -->
-  <div class="jumbotron jumbotron-fluid feature" id="feature-first">
-    <div class="container my-5">
-        <div class="row justify-content-between text-center text-md-left">
-            <div data-aos="fade-right" data-aos-duration="1000" data-aos-once="true" class="col-md-6">
-                <h2 class="font-weight-bold">Take a look inside</h2>
-                <p class="my-4">Te iisque labitur eos, nec sale argumentum scribentur no,
-                    <br> augue disputando in vim. Erat fugit sit at, ius lorem deserunt deterruisset no.</p>
-                <a href="#" class="btn my-4 font-weight-bold atlas-cta cta-blue">Learn More</a>
-            </div>
-            <div data-aos="fade-left" data-aos-duration="1000" data-aos-once="true" class="col-md-6 align-self-center">
-                <img src="{{asset('frontend/assets/images/cars/car1.png')}}" alt="Take a look inside" class="mx-auto d-block">
-            </div>
-        </div>
-    </div>
-</div>
+  @include('frontend.components.servicetop')
 <!-- feature (green background) -->
   <main id="main">
     <!-- ======= heading Section ======= -->
