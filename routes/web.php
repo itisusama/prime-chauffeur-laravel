b0a1fd46f92bda34db2f86482a581191f0ed0011
Usama Hassan <ahmarkhan1233456@gmail.com>
2024-01-25 14:04:31 +0500

    Route Services grouping

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,11 @@ Route::view('/blog', 'frontend.blog')->name('blog');
 Route::view('/singleblog', 'frontend.singleblog')->name('singleblog');
 Route::view('/cars', 'frontend.cars')->name('cars');
 Route::view('/booking', 'frontend.booking')->name('booking');
-
-Route::view('/airport', 'frontend.airport')->name('airport');
-Route::view('/wedding', 'frontend.wedding')->name('wedding');
-Route::view('/tours', 'frontend.tours')->name('tours');
-Route::view('/events', 'frontend.events')->name('events');
-Route::view('/business', 'frontend.business')->name('business');
+// Services Group
+Route::group(['prefix' => 'services'], function () {
+    Route::view('/airport', 'frontend.airport')->name('airport');
+    Route::view('/wedding', 'frontend.wedding')->name('wedding');
+    Route::view('/tours', 'frontend.tours')->name('tours');
+    Route::view('/events', 'frontend.events')->name('events');
+    Route::view('/business', 'frontend.business')->name('business');
+});
